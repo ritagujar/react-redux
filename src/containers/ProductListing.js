98cd5244058e1eac7aae4b153a955131a4d4d6e3
Rita-Gujar <ritagujar14@gmail.com>
2021-08-03 21:37:45 +0530

    Used axios for redux API call

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 function ProductListing() {
   // To fetch the products which are in redux store we use useSelector
   const products = useSelector((state) => state);
-  console.log(products);
+
+  // Function for fetching all the products from API
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((error) => {
+        console.log("Error", error);
+      });
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products.data);
   return (
     <div className="ui grid container">
       <ProductComponent />
